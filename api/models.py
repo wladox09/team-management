b406a045f16058b1cde4cdc9bd75c8a4eeef18fc
Wladimir Campos <wladimir09@gmail.com>
2022-03-01 17:40:03 -0400

    implementado email parte 1

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.dispatch import receiver
 from model_utils.models import TimeStampedModel
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 
 class User(AbstractUser):
@@ -17,13 +19,20 @@ class Team(TimeStampedModel):
         auto_now_add=True, verbose_name="date published")
     date_updated = models.DateTimeField(
         auto_now=True, verbose_name="date updated")
-    members = models.ManyToManyField(User, through='Member', related_name='teams')
+    members = models.ManyToManyField(
+        User, through='Member', related_name='teams')
 
     def __str__(self):
         return self.name
 
-
 class Member(models.Model):
-    user = models.ForeignKey(User, on_delete=models.Case)
-    team = models.ForeignKey(Team, on_delete=models.Case)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
     date_joined = models.DateField()
+
+
+@receiver(post_save, sender=Team)
+def send_mail(sender, instance, created, **kwargs):
+    if created:
+        #Busar admins y notificar por email
+        print(instance)
